48e85e1c82d91d8a47dabc44d172fd7166d9ecde
Abram-An <anwerabram@gmail.com>
2026-03-12 17:54:05 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'screens/splash_screen.dart';
 import 'screens/onboarding_page.dart';
-import 'screens/home_screen.dart';
+import 'screens/main_navigation.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,14 +15,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'AI Study App',
+
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true, // استخدام Material 3
+        useMaterial3: true,
       ),
-      initialRoute: '/', // البداية على Onboarding
+
+      /// أول شاشة تظهر
+      home: const SplashScreen(),
+
       routes: {
-        '/': (context) => const OnboardingScreen(),
-        '/home': (context) => const HomeScreen(),
+        '/onboarding': (context) => const OnboardingScreen(),
+        '/home': (context) => const MainNavigation(),
+      },
+
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (context) => const OnboardingScreen(),
+        );
       },
     );
   }
